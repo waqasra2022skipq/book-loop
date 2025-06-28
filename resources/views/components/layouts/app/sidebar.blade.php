887eafb9da64eb5430088187fabd7c8d711e579d
Waqas Rahman <waqasrehman@invotyx.co.uk>
2025-06-28 21:12:25 +0500

    Add MyBooks route and sidebar link for user-owned books

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -15,6 +15,7 @@
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
                     <flux:navlist.item icon="" :href="route('books.create')" :current="request()->routeIs('books.create')" wire:navigate>{{ __('Add a Book') }}</flux:navlist.item>
+                    <flux:navlist.item icon="" :href="route('books.mybooks')" :current="request()->routeIs('books.mybooks')" wire:navigate>{{ __('My Books') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\CreateBook;
+use App\Livewire\MyBooks;
+use App\Livewire\EditBookInstance;
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,6 +24,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 
     Route::get('books/create', CreateBook::class)->name('books.create');
+    Route::get('books/mybooks', MyBooks::class)->name('books.mybooks');
+    Route::get('books/edit/{bookid}', EditBookInstance::class)->name('books.editBook');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
