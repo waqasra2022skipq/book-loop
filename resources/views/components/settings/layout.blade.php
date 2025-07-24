09dc90e8c8b92b4343b289bf2624205339b88971
waqasra2022skipq <waqas.rahman.sculptor@gmail.com>
2025-07-24 15:23:46 +0500

    Refactor header and head components: remove dark class from HTML tag and comment out fluxAppearance directive

--- resources/views/components/settings/layout.blade.php
+++ resources/views/components/settings/layout.blade.php
@@ -4,7 +4,7 @@
             <flux:navlist>
                 <flux:navlist.item :href="route('settings.profile')" wire:navigate>{{ __('Profile') }}</flux:navlist.item>
                 <flux:navlist.item :href="route('settings.password')" wire:navigate>{{ __('Password') }}</flux:navlist.item>
-                <flux:navlist.item :href="route('settings.appearance')" wire:navigate>{{ __('Appearance') }}</flux:navlist.item>
+                {{-- <flux:navlist.item :href="route('settings.appearance')" wire:navigate>{{ __('Appearance') }}</flux:navlist.item> --}}
             </flux:navlist>
         </div>
 
