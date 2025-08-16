3363f6c7f332803fcb4710de6a81018915cab0bb
Waqas Rahman <104453900+waqasra2022skipq@users.noreply.github.com>
2025-08-16 23:15:42 +0500

    refactor: Enhance SEO and metadata for book instance and books pages

--- app/Livewire/BookInstance.php
+++ app/Livewire/BookInstance.php
@@ -20,6 +20,9 @@ class BookInstance extends Component
         // For example, you might want to fetch the book by ID from the route parameter
 
 
-        return view('livewire.book-instance');
+        return view('livewire.book-instance')->layoutData([
+            'title' => "Book Instance - {$this->bookInstance->book->title}",
+            'description' => "Read '{$this->bookInstance->book->title}' by {$this->bookInstance->book->author}.",
+        ]);
     }
 }
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,20 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <title>{{ $title ?? config('app.name') }}</title>
+<meta name="description" content="{{ $description ?? 'Explore, Borrow, Lend Books for free' }}">
+
+{{-- Open Graph Meta Tags --}}
+<meta property="og:title" content="{{ $title ?? config('app.name') }}">
+<meta property="og:description" content="{{ $description ?? 'Explore, Borrow, Lend Books for free' }}">
+<meta property="og:image" content="{{ $ogImage ?? asset('images/logo.svg') }}" />
+<meta property="og:url" content="{{ request()->url() }}">
+<meta property="og:type" content="website">
+
+{{-- Twitter Card Meta Tags --}}
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="{{ $title ?? config('app.name') }}">
+<meta name="twitter:description" content="{{ $description ?? 'Explore, Borrow, Lend Books for free' }}">
+<meta name="twitter:image" content="{{ $ogImage ?? asset('images/logo.svg') }}">
 
 <link rel="icon" href="{{ asset('images/book_icon.png') }}" sizes="any">
 <link rel="icon" href="{{ asset('images/book_icon.png') }}" type="image/svg+xml">
