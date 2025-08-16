a9948880c43ca838992688978ba36803d1b60499
Waqas Rahman <104453900+waqasra2022skipq@users.noreply.github.com>
2025-08-16 23:34:50 +0500

    refactor: Remove unnecessary class from app logo component for cleaner markup

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,1 +1,1 @@
-<flux:brand href="{{ route('welcome') }}" logo="{{ asset('images/logo.svg') }}" class="max-lg:hidden justify-center " />
+<flux:brand href="{{ route('welcome') }}" logo="{{ asset('images/logo.svg') }}" class="justify-center" />
